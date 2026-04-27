6fcf1690ade5c9b6f44132340b0354298db0acd8
Meyneth <frr_gufi@web.de>
2026-04-27 16:58:58 +0200

    fixed manifest path

--- symptom-navigator/app/layout.tsx
+++ symptom-navigator/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Symptometer",
   description: "asses your symptoms",
+  manifest: "/manifest.json"
 };
 
 export default function RootLayout({
